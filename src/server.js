f96e83aabe8172ddb406d1d1805eb8e66b17a35f
Edson Carlos <edsonsilva@sp.gov.br>
2019-10-04 16:12:52 -0300

    Full backend

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/omnistack9', {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
